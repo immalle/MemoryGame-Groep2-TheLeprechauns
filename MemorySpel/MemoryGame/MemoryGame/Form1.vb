c78c24bb8c32752647d1d87f818bc19bc614b3e8
Yves Bos <yves.bos@immalle.be>
2013-03-14 11:35:19 +0100

    VB Project Upload

---
+++ MemorySpel/MemoryGame/MemoryGame/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class MemoryGame
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.BackgroundImage = My.Resources.Wallpaper
+    End Sub
+End Class
