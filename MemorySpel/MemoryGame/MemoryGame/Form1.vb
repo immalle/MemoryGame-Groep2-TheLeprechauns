f881cabf024294efa9dc7436c39166d5a681cac1
Yves Bos <Yves.bos@immalle.be>
2013-03-16 13:11:59 +0100

    ...

--- MemorySpel/MemoryGame/MemoryGame/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class MemoryGame
-
-    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Me.BackgroundImage = My.Resources.Wallpaper
-    End Sub
-End Class
